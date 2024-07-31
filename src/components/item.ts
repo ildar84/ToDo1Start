cfa5a2a31a55d9b97d99de8da4561667febd592b
Ildar <isuncheleev@yandex.ru>
2024-07-31 21:35:44 +0300

    310724

--- src/components/item.ts
+++ src/components/item.ts
@@ -1,6 +1,22 @@
-export function createItem(name: string) {
-    const itemElement = template.content.querySelector('.todo-item').cloneNode(true) as HTMLElement;
-    const title = itemElement.querySelector('.todo-item__text')
-    title.textContent = name;
-    return itemElement;
+// export function createItem(template: HTMLTemplateElement, name: string) {
+//     const itemElement = template.content.querySelector('.todo-item').cloneNode(true) as HTMLElement;
+//     const title = itemElement.querySelector('.todo-item__text')
+//     title.textContent = name;
+//     return itemElement;
+// }
+
+export class Item {
+    
+    protected itemElement: HTMLElement;
+    protected title: HTMLElement;
+
+    constructor (template: HTMLTemplateElement) {
+        this.itemElement = template.content.querySelector('.todo-item').cloneNode(true) as HTMLElement;
+        this.title = this.itemElement.querySelector('.todo-item__text');
+    }
+
+    render(item: string) {
+        this.title.textContent = item;
+        return this.itemElement;    
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+import { Item } from "./components/Item";
 import "./styles/styles.css"
-import { todos } from "./utils/constants"
+import { todos } from "./utils/constants";
 
 const contentElement = document.querySelector('.todos__list')
 
@@ -8,6 +9,9 @@ const template = document.querySelector('#todo-item-template') as HTMLTemplateEl
 
 
 todos.forEach( item => {
-    const itemElement = createItem(item)
+    // const itemElement = createItem(template, item)
+    // contentElement.prepend(itemElement);
+    const todoItem = new Item(template);
+    const itemElement = todoItem.render(item);
     contentElement.prepend(itemElement);
 })
